31e24d83895078dcfc288d2dcb0e63ccf4c9d30e
Aadit22 <84570501+Aadit22@users.noreply.github.com>
2022-02-03 14:45:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -31,3 +31,75 @@ function new_image (get_image){
         canvas.add(block_object);
     });
 }
+
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    keypress=e.keyCode;
+    console.log(keypress);
+    if(e.shiftKey==true && keypress=='80')
+    {
+        console.log("p&shiftpresstogether");
+        block_width=block_width+10;
+        block_height=block_height+10;
+        document.getElementById("current_width").innerHTML=block_width;
+        document.getElementById("current_height").innerHTML=block_height; 
+    }
+    if(e.shiftKey==true && keypress=='77')
+    {
+        console.log("m&shiftpresstogether");
+        block_width=block_width-10;
+        block_height=block_height-10;
+        document.getElementById("current_width").innerHTML=block_width;
+        document.getElementById("current_height").innerHTML=block_height;
+    }
+    if(keypress=='38')
+    {
+        up();
+        console.log("up");
+    }
+    if(keypress=='40')
+    {
+        down();
+        console.log("down");
+    }
+    if(keypress=='37')
+    {
+        left();
+        console.log("left");
+    }
+    if(keypress=='39')
+    {
+        right();
+        console.log("right");
+    }
+    if(keypress=='87')
+    {
+        new_image('wall.jpg');
+        console.log("w");
+    }
+    if(keypress=='71')
+    {
+        new_image('ground.png');
+        console.log("g");
+    }
+    if(keypress=='76')
+    {
+        new_image('light_green.png');
+        console.log("l");
+    }
+    if(keypress=="84")
+    {
+        new_image('trunk.jpg');
+        console.log("t");
+    }
+    if(keypress=='82')
+    {
+        new_image('roof.jpg');
+        console.log("r");
+    }
+    if(keypress=='89')
+    {
+        new_image('yellow_wall.png');
+        console.log("y");
+    }
+}
